39426529547536d2103303fe581278392cd9507c
scyllacore <yms8795@gmail.com>
2023-08-09 14:21:16 +0900

    chore : create project

---
+++ src/main/java/com/incruit/aggregateWeb/AggregateWebApplication.java
@@ -0,0 +1,13 @@
+package com.incruit.aggregateWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregateWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incruit/aggregateWeb/AggregateWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incruit.aggregateWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregateWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
