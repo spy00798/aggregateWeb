776c8115c5312fcf885541267bf06ea09dc287c2
scyllacore <yms8795@gmail.com>
2023-08-11 20:49:03 +0900

    feat : thymeleaf 작동 확인을 위한 간단한 샘플 코드 작성.

--- src/main/java/com/incruit/aggregateWeb/controller/IndexController.java
+++ src/main/java/com/incruit/aggregateWeb/controller/IndexController.java
@@ -3,14 +3,12 @@ package com.incruit.aggregateWeb.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/")
     public String index(HttpServletRequest request){
-        return "Hello index";
+        return "content/index";
     }
 
 }
