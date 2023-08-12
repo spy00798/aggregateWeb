53e1bae1f9a69732f312faeaf4712bca21399174
scyllacore <yms8795@gmail.com>
2023-08-12 22:18:59 +0900

    feat : 기본 controller 구현.

--- src/main/java/com/incruit/aggregateWeb/controller/IndexController.java
+++ src/main/java/com/incruit/aggregateWeb/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.incruit.aggregateWeb.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.incruit.aggregateWeb.db.dto.QAdto;
+import com.incruit.aggregateWeb.service.QAservice;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final QAservice qAservice;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
-        return "content/index";
+    public String index(Model model, QAdto qAdto){
+        return qAservice.getQAList(model,qAdto);
     }
 
 }
