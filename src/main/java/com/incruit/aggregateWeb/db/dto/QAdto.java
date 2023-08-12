bf41931eff23ea2d2972fc082c36c913d228c45e
scyllacore <yms8795@gmail.com>
2023-08-12 22:18:12 +0900

    feat : dto 구현.

---
+++ src/main/java/com/incruit/aggregateWeb/db/dto/QAdto.java
@@ -0,0 +1,13 @@
+package com.incruit.aggregateWeb.db.dto;
+
+
+import lombok.Data;
+
+@Data
+public class QAdto {
+
+    private long id;
+    private String question;
+    private String answer;
+
+}
